a941eb035b9093ccbd5c386e27ed88d897df5e1a
Christof Kaller <Christof.Kaller@gmail.com>
2022-10-17 14:05:33 +0200

    changes

--- app.js
+++ app.js
@@ -4,16 +4,35 @@ const authRouts = require("./routes/auth");
 const app = express();
 const { appLogger } = require("./utils/logger");
 const port = 3000;
+var fs = require("fs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/auth", authRouts);
-app.use("/api", userRouts);
-
 // Root path
 app.get("/", (req, res, next) => {
-  res.json({ message: "Ok" });
+  res.json({ message: "Okese" });
+  appLogger.info(`Server got : ${req.url}!`);
+});
+
+// API path to get current log
+app.get("/api/getlog", (req, res, next) => {
+  var readable = fs.createReadStream("./logs/onlineLog.log");
+  readable.pipe(res);
+  return;
+});
+
+// Root path
+app.get("/logviewer", (req, res, next) => {
+  fs.readFile("./utils/logs.html", function (err, data) {
+    if (err) {
+      appLogger.info(`Server gotss : ${err}!` + err);
+      throw err;
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+    return;
+  });
 });
 
 app.listen(port, () => {
